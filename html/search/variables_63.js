024d5b8399d95c1e6199c5364d9359a59ed9fb18
alexandra27 <martinezalexandra_27@yahoo.com>
2015-02-05 16:42:12 -0600

    Commiting all the required files for submission

---
+++ html/search/variables_63.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cache',['cache',['../Collatz_8c_09_09.html#a9d60f05747bb0071558d9b4093be162a',1,'cache():&#160;Collatz.c++'],['../SphereCollatz_8c_09_09.html#a9d60f05747bb0071558d9b4093be162a',1,'cache():&#160;SphereCollatz.c++']]]
+];
